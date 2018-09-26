4af3442964e1a040aeb6e56093cac9c2caf24faf
Jilmar <jtaj@cin.ufpe.br>
2018-09-26 10:37:59 -0300

    ItemPedido

--- src/main/java/com/jilmar/cursomc/CursomcApplication.java
+++ src/main/java/com/jilmar/cursomc/CursomcApplication.java
@@ -13,6 +13,7 @@ import com.jilmar.cursomc.domain.Cidade;
 import com.jilmar.cursomc.domain.Cliente;
 import com.jilmar.cursomc.domain.Endereco;
 import com.jilmar.cursomc.domain.Estado;
+import com.jilmar.cursomc.domain.ItemPedido;
 import com.jilmar.cursomc.domain.Pagamento;
 import com.jilmar.cursomc.domain.PagamentoComBoleto;
 import com.jilmar.cursomc.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.jilmar.cursomc.repositories.CidadeRepository;
 import com.jilmar.cursomc.repositories.ClienteRepository;
 import com.jilmar.cursomc.repositories.EnderecoRepository;
 import com.jilmar.cursomc.repositories.EstadoRepository;
+import com.jilmar.cursomc.repositories.ItemPedidoRepository;
 import com.jilmar.cursomc.repositories.PagamentoRepository;
 import com.jilmar.cursomc.repositories.PedidoRepository;
 import com.jilmar.cursomc.repositories.ProdutoRepository;
@@ -55,6 +57,9 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -119,5 +124,18 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.0, 1, 2000.0);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.0, 2, 80.0);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.0, 1, 800.0);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 	}
 }
--- src/main/java/com/jilmar/cursomc/domain/Pedido.java
+++ src/main/java/com/jilmar/cursomc/domain/Pedido.java
@@ -2,6 +2,8 @@ package com.jilmar.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -10,6 +12,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -32,6 +35,9 @@ public class Pedido implements Serializable{
 	@ManyToOne
 	@JoinColumn(name="endereco_de_entrega_id")
 	private Endereco enderecoDeEntrega;
+	
+	@OneToMany(mappedBy="id.pedido")
+	private Set<ItemPedido> itens = new HashSet<ItemPedido>();
 
 	public Integer getId() {
 		return id;
@@ -72,6 +78,14 @@ public class Pedido implements Serializable{
 	public void setEnderecoDeEntrega(Endereco enderecoDeEntrega) {
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
+	
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
 
 	public Pedido(Integer id, Date instante, Cliente cliente, Endereco enderecoDeEntrega) {
 		super();
@@ -108,7 +122,5 @@ public class Pedido implements Serializable{
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}
-	
-	
+	}	
 }
