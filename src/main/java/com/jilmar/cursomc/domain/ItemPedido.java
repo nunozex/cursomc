e1f1bc23b0fb48f8f40f35ffc5878cf361584779
Jilmar <jtaj@cin.ufpe.br>
2018-09-26 11:05:52 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/jilmar/cursomc/domain/ItemPedido.java
+++ src/main/java/com/jilmar/cursomc/domain/ItemPedido.java
@@ -5,11 +5,14 @@ import java.io.Serializable;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 public class ItemPedido implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	
+	@JsonIgnore
 	@EmbeddedId
 	ItemPedidoPK id = new ItemPedidoPK();
 	
@@ -62,6 +65,7 @@ public class ItemPedido implements Serializable{
 		this.id = id;
 	}
 	
+	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
