5e27af19057c6a16e1b9a7fa69e38a80ca2d0f5c
Jilmar <jtaj@cin.ufpe.br>
2018-09-28 10:30:48 -0300

    Validacao custoizada: email nao repitodo na insercao de Cliente

--- src/main/java/com/jilmar/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/jilmar/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.jilmar.cursomc.domain.Cliente;
 import com.jilmar.cursomc.domain.enums.TipoCliente;
 import com.jilmar.cursomc.dto.ClienteNewDTO;
+import com.jilmar.cursomc.repositories.ClienteRepository;
 import com.jilmar.cursomc.resources.exception.FieldMessage;
 import com.jilmar.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -19,7 +27,7 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 	@Override
 	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
 		List<FieldMessage> list = new ArrayList<>();
-
+		
 		// inclua os testes aqui, inserindo erros na lista
 		
 		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpf())) {
@@ -28,6 +36,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpf())) {
 			list.add(new FieldMessage("Cpf", "CNPJ invalido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("email", "Email ja existente"));
 		}
 
 		for (FieldMessage e : list) {
